cce9a7d78feed4bac11f0f7aedbfe8fa1d8ff037
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2022-03-31 09:00:47 +0100

    .

--- AL02/exc3/pacientes2-online.php
+++ AL02/exc3/pacientes2-online.php
@@ -9,31 +9,35 @@
 </head>
 <body>
   <?php 
-    $xml = simplexml_load_file("pacientes.xml");
+    $xml = simplexml_load_file("pacientes2.xml");
     echo "<h2>Pacientes</h2><br />";
 
     echo "<table border='1'>";
-    echo "<tr><td>ID</td><td>Sexo</td><td>Ano Nascimento</td><td>BI</td><td>Contribuinte</td><td>Sistema de Saúde</td><td>Doutor</td></tr>";
+    echo "<tr><td>ID</td><td>Sexo</td><td>Nome</td><td>Ano Nascimento</td><td>BI</td><td>Contribuinte</td><td>Sistema de Saúde</td><td>Doutor</td></tr>";
     foreach($xml->children() as $paciente)
-    {  
-      if((date("Y") - $paciente->anoNascimento) < 18){
-        echo "<tr>";  
-        echo "<td>".$paciente->id."</td>";
-        echo "<td>".$paciente->sexo."</td>";
-        echo "<td>".$paciente->anoNascimento."</td>";
-        echo "<td>".$paciente->bi."</td>";
-        echo "<td>".$paciente->contribuinte."</td>";
-        echo "<td>".$paciente->sistemaSaude."</td>";
-        if($paciente->sexo == "M"){
-          echo "<td>Dr.Marcelo Pereira</td>";
-        }
-        else{
-          echo "<td>Dra.Josefina Fontes</td>";
-        }
-        echo "</tr>";
-      }      
+    {          
+        $paciente->addChild("doutor");
+        if($paciente->attributes()->sexo == "M")
+          $paciente->doutor = "Dr.Marcelo Pereira";
+        else
+          $paciente->doutor = "Dra.Josefina Fontes";
+    
+
+        if((date("Y") - $paciente->anoNascimento) < 18){
+          echo "<tr>";  
+          echo "<td>".$paciente->attributes()->id."</td>";
+          echo "<td>".$paciente->attributes()->sexo."</td>";
+          echo "<td>".$paciente->name."</td>";
+          echo "<td>".$paciente->anoNascimento."</td>";
+          echo "<td>".$paciente->bi."</td>";
+          echo "<td>".$paciente->contribuinte."</td>";
+          echo "<td>".$paciente->sistemaSaude."</td>";
+          echo "<td>".$paciente->doutor."</td>";
+          echo "</tr>";  
+        }     
     }
     echo "</table>";
+    $xml->asXML('pacientes2final.xml');
   ?>
 </body>
 </html>
