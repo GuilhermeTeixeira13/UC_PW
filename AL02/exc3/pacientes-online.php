2bc60cc52673319d71ceb4007493f73438796a38
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2022-04-06 17:28:43 +0100

    modificações no AL02

--- AL02/exc3/pacientes-online.php
+++ AL02/exc3/pacientes-online.php
@@ -13,19 +13,37 @@
     echo "<h2>Pacientes</h2><br />";
 
     echo "<table border='1'>";
-    echo "<tr><td>ID</td><td>Sexo</td><td>Ano Nascimento</td><td>BI</td><td>Contribuinte</td><td>Sistema de Saúde</td></tr>";
+    echo "<tr><td>ID</td><td>Sexo</td><td>Nome</td><td>Ano Nascimento</td><td>BI</td><td>Contribuinte</td><td>Sistema de Saúde</td><td>Doutor</td></tr>";
     foreach($xml->children() as $paciente)
-    {  
-      echo "<tr>";  
-      echo "<td>".$paciente->id."</td>";
-      echo "<td>".$paciente->sexo."</td>";
-      echo "<td>".$paciente->anoNascimento."</td>";
-      echo "<td>".$paciente->bi."</td>";
-      echo "<td>".$paciente->contribuinte."</td>";
-      echo "<td>".$paciente->sistemaSaude."</td>";
-      echo "</tr>";      
+    { 
+      $paciente->addAttribute('id', $paciente->id);
+      unset($paciente->id);
+      $paciente->addAttribute('sexo', $paciente->sexo);
+      unset($paciente->sexo);
+      
+      $paciente->addChild("doutor");
+      if($paciente->attributes()->sexo == 'M')
+        $paciente->doutor = "Dr.Marcelo Pereira";
+      else
+        $paciente->doutor = "Dra.Josefina Fontes";
+    
+      if((date("Y") - $paciente->anoNascimento) < 18){
+        echo "<tr>";  
+        echo "<td>".$paciente->attributes()->id."</td>";
+        echo "<td>".$paciente->attributes()->sexo."</td>";
+        echo "<td>".$paciente->name."</td>";
+        echo "<td>".$paciente->anoNascimento."</td>";
+        echo "<td>".$paciente->bi."</td>";
+        echo "<td>".$paciente->contribuinte."</td>";
+        echo "<td>".$paciente->sistemaSaude."</td>";
+        echo "<td>".$paciente->doutor."</td>";
+        echo "</tr>";  
+      } 
     }
     echo "</table>";
+
+    
+    $xml->asXML('pacientes2.xml');
   ?>
 </body>
 </html>
